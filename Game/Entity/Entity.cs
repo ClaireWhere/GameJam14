2bdb325bae02d4b642aa8d13edc22cc2960847eb
ClaireWhere <clairewhere4@gmail.com>
2024-01-15 14:48:44 -0500

    feat: add properties

--- Game/Entity/Entity.cs
+++ Game/Entity/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,4 +7,32 @@ using System.Threading.Tasks;
 
 namespace GameJam14.Game.Entity;
 internal class Entity {
+    public int Id { get; set; }
+    public Vector2 Position { get; set; }
+    public Vector2 Velocity { get; set; }
+    public Vector2 Acceleration { get; set; }
+    public CollisionSource Collision { get; set; }
+    public Sprite Sprite { get; set; }
+
+    public Entity(int id, Vector2 position, Vector2 velocity, Vector2 acceleration, CollisionSource collision, Sprite sprite ) {
+        this.Id = id;
+        this.Position = position;
+        this.Velocity = velocity;
+        this.Acceleration = acceleration;
+        this.Collision = collision;
+        this.Sprite = sprite;
+    }
+
+    /// <summary>
+    /// Updates the sprite's position based on its velocity and acceleration.
+    /// </summary>
+    /// <param name="gameTime">The game time.</param>
+    public virtual void Update(GameTime gameTime) {
+        this.Position += ( this.Velocity * (float) gameTime.ElapsedGameTime.TotalSeconds ) + ( this.Acceleration * (float) Math.Pow(gameTime.ElapsedGameTime.TotalSeconds, 2) / 2 );
+        this.Velocity += this.Acceleration * (float) gameTime.ElapsedGameTime.TotalSeconds;
+    }
+
+    public virtual void Kill() {
+
+    }
 }
