7c1bf45ccf393194708f6ba46be1b0aea0900721
ClaireWhere <clairewhere4@gmail.com>
2024-01-15 14:49:28 -0500

    feat: add properties and constructor

--- Game/Entity/Bullet.cs
+++ Game/Entity/Bullet.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,4 +7,36 @@ using System.Threading.Tasks;
 
 namespace GameJam14.Game.Entity;
 internal class Bullet : Entity {
+    public int Power { get; set; }
+    public double TimeToLive { get; set; }
+    public double TimeAlive { get; set; }
+    /// <summary>
+    /// The death effect when this bullet dies.
+    /// Default : The bullet will disappear.
+    /// Explode : The bullet will explode in a cloud effect.
+    /// None    : The bullet will never die.
+    /// </summary>
+    public enum DeathEffect {
+        Default,
+        Explode,
+        None
+    }
+    public DeathEffect Death { get; set; }
+
+    public Bullet(int id, Vector2 position, Vector2 velocity, Vector2 acceleration, Sprite sprite, bool hitsPlayer, bool hitsEnemy, int power, double timeToLive, DeathEffect deathEffect)
+            : base(id,
+                position,
+                velocity,
+                acceleration,
+                new CollisionSource(
+                    type: new CollisionType(CollisionType.SolidType.NonSolid, CollisionType.LightType.None, CollisionType.EntityType.Other, hitsPlayer, hitsEnemy),
+                    hitbox: new List<Shape.Shape>() { new Shape.Rectangle(position, sprite.Texture.Width, sprite.Texture.Height) }
+                ),
+                sprite
+            ) {
+        this.Power = power;
+        this.TimeToLive = timeToLive;
+        this.TimeAlive = 0;
+        this.Death = deathEffect;
+    }
 }
--- Game/Entity/Cloud.cs
+++ Game/Entity/Cloud.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,4 +7,35 @@ using System.Threading.Tasks;
 
 namespace GameJam14.Game.Entity;
 internal class Cloud : Entity {
+    public float ExpansionSpeed { get; set; }
+    public float ExpansionAcceleration { get; set; }
+    public float MaxExpansion { get; set; }
+    public int Damage { get; set; }
+    public double DamageDegredation { get; set; }
+
+    public float Expansion { get; set; }
+    public float HitboxRadius { get; set; }
+
+    // int id, Vector2 position, Vector2 velocity, Vector2 acceleration, CollisionSource collision, Sprite sprite
+    public Cloud(int id, Vector2 position, Sprite sprite, float expansionSpeed, float expansionAcceleration, float maxExpansion, int damage, double damageDegradation)
+            : base(
+                id: id,
+                position: position,
+                velocity: Vector2.Zero,
+                acceleration: Vector2.Zero,
+                collision: new CollisionSource(
+                    type: new CollisionType(CollisionType.SolidType.NonSolid,
+                        CollisionType.LightType.None,
+                        CollisionType.EntityType.Other, false, false),
+                    hitbox: new List<Shape.Shape>() { new Shape.Circle(position, sprite.Texture.Width) }),
+                sprite: sprite
+            ) {
+        this.ExpansionSpeed = expansionSpeed;
+        this.ExpansionAcceleration = expansionAcceleration;
+        this.Expansion = 0f;
+        this.MaxExpansion = maxExpansion;
+        this.Damage = damage;
+        this.DamageDegredation = damageDegradation;
+        this.HitboxRadius = sprite.Texture.Width;
+    }
 }
