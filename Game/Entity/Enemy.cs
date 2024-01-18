5c5cb2091facd81834edf7a50150b5ddad2b008f
ClaireWhere <clairewhere4@gmail.com>
2024-01-17 22:54:40 -0500

    feat: add constructor

--- Game/Entity/Enemy.cs
+++ Game/Entity/Enemy.cs
@@ -6,4 +6,24 @@ using System.Threading.Tasks;
 
 namespace GameJam14.Game.Entity;
 internal class Enemy : EntityActor {
+    public Target Target { get; set; }
+
+    public Enemy(int id, string name, Vector2 position, List<Shape.Shape> hitbox, Sprite sprite, Stats baseStats, Inventory inventory, Attack attack, Target target)
+        : base(
+            id: id,
+            name: name,
+            position: position,
+            collision:
+                new CollisionSource(
+                    type: new CollisionType(CollisionType.SolidType.Solid, CollisionType.LightType.None, CollisionType.EntityType.Enemy, true, false),
+                    hitbox: hitbox
+                ),
+            sprite: sprite,
+            baseStats: baseStats,
+            inventory: inventory,
+            attack: attack
+        ) {
+        this.Target = target;
+    }
+
 }
