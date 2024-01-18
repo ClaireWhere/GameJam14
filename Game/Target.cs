d3cff829d4228f6657e29bac9716688f3f200bb4
ClaireWhere <clairewhere4@gmail.com>
2024-01-17 22:50:46 -0500

    feat: add Target

---
+++ Game/Target.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam14.Game;
+internal class Target {
+
+    /// <summary>
+    /// Types of targets for the enemy to attack.
+    /// </summary>
+    public enum TargetType {
+        Player,
+        Light,
+        None
+    }
+    /// <summary>
+    /// Gets or sets the target type.
+    /// </summary>
+    public TargetType Type { get; set; }
+    /// <summary>
+    /// Gets or sets the target range.
+    /// </summary>
+    public float TargetRange { get; set; }
+    /// <summary>
+    /// Gets or sets the target distance.
+    /// </summary>
+    public float TargetDistance { get; set; }
+    /// <summary>
+    /// Gets or sets the target angle.
+    /// </summary>
+    public float TargetAngle { get; set; }
+
+    public Target(TargetType type, float targetRange, float targetDistance, float targetAngle) {
+        Type = type;
+        TargetRange = targetRange;
+        TargetDistance = targetDistance;
+        TargetAngle = targetAngle;
+    }
+
+    public Target() {
+        Type = TargetType.None;
+        TargetRange = 0;
+        TargetDistance = 0;
+        TargetAngle = 0;
+    }
+}
