a6f010f630533e65a84baa7b8d6466e3a0a697e2
ClaireWhere <clairewhere4@gmail.com>
2024-01-17 22:41:09 -0500

    feat: add Modifiers

---
+++ Game/Modifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam14.Game;
+internal class Modifier {
+    public Stats Stats { get; private set; }
+    private float Duration { get; set; }
+    private float TimeRemaining { get; set; }
+
+    public Modifier(Stats stats, float duration) {
+        Stats = stats;
+        Duration = duration;
+        TimeRemaining = duration;
+    }
+
+    public void Update(float deltaTime) {
+        TimeRemaining -= deltaTime;
+    }
+
+    public bool IsExpired() {
+        return TimeRemaining <= 0;
+    }
+
+}
