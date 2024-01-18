13d396b1a12f1ee742cd5dd587d86727ba019561
ClaireWhere <clairewhere4@gmail.com>
2024-01-17 22:40:53 -0500

    feat: add TextureBundle
    Add TextureBundle class to bundle together multiple textures to be used by the Sprite class

---
+++ Game/TextureBundle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameJam14.Game;
+internal class TextureBundle {
+
+
+    /// <summary>
+    /// Gets the textures of this bundle.
+    /// </summary>
+    private Dictionary<TextureType, Texture2D> _textures { get; }
+    /// <summary>
+    /// Gets or sets the current texture state of this bundle.
+    /// </summary>
+    private TextureType _currentState { get; set; }
+
+    public TextureType CurrentState {
+        get {
+            return this._currentState;
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TextureBundle"/> class.
+    /// </summary>
+    /// <param name="textures">The textures.</param>
+    /// <param name="initialState">The initial state.</param>
+    public TextureBundle(Dictionary<TextureType, Texture2D> textures, TextureType? initialState = null) {
+        this._textures = textures;
+        this._currentState = initialState ?? textures.Keys.First();
+    }
+
+    /// <summary>
+    /// Gets the current texture of this bundle.
+    /// </summary>
+    public Texture2D Texture {
+        get {
+            return this._textures[this._currentState];
+        }
+    }
+
+    public void SetState(TextureType state) {
+        if (this.HasTexture(state)) {
+            this._currentState = state;
+        }
+    }
+
+    private bool HasTexture(TextureType type) {
+        return this._textures.ContainsKey(type);
+    }
+
+    private Texture2D GetTexture(TextureType type) {
+        if (!this.HasTexture(type)) {
+            return null;
+        }
+        return this._textures[type];
+    }
+
+    private Texture2D GetTexture(string type) {
+        return this.GetTexture((TextureType) Enum.Parse(typeof(TextureType), type));
+    }
+
+    private Texture2D GetTexture(int type) {
+        return this.GetTexture((TextureType) type);
+    }
+
+    /// <summary>
+    /// Sets the texture for the specified type.
+    /// </summary>
+    /// <param name="type">The texture type.</param>
+    /// <param name="texture">The texture.</param>
+    public void SetTexture(TextureType type, Texture2D texture) {
+        if (this.HasTexture(type)) {
+            this._textures[type] = texture;
+        } else {
+            this._textures.Add(type, texture);
+        }
+    }
+
+}
---
+++ Game/TextureType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam14.Game;
+public enum TextureType {
+    FaceRight,
+    FaceLeft,
+    FaceFront,
+    FaceBack,
+    ExpressionShock,
+    ExpressionRizz,
+    ExpressionUnsure,
+    ExpressionExcited,
+    ExpressionAngry,
+    ExpressionSad,
+    ExpressionHappy,
+    ExpressionNeutral,
+}
