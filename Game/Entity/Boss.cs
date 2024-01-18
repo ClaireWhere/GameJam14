246a9d249870ef3a451c622e31b80a94af2a56b2
ClaireWhere <clairewhere4@gmail.com>
2024-01-17 22:54:48 -0500

    feat: add constructor

--- Game/Entity/Boss.cs
+++ Game/Entity/Boss.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Ignore Spelling: hitbox
+
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,4 +9,17 @@ using System.Threading.Tasks;
 
 namespace GameJam14.Game.Entity;
 internal class Boss : Enemy {
+    public Boss(int id, string name, Vector2 position, List<Shape.Shape> hitbox, Sprite sprite, Stats stats, Target target)
+        : base(
+            id: id,
+            name: name,
+            position: position,
+            hitbox: hitbox,
+            sprite: sprite,
+            baseStats: stats,
+            inventory: new Inventory(),
+            attack: new Attack(),
+            target: target
+        ) {
+    }
 }
