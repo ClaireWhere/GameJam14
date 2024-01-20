3e205aa277a4394023125f46e33866d660410db6
ClaireWhere <clairewhere4@gmail.com>
2024-01-20 10:55:40 -0500

    feat: add UpdateTargets

--- Game/EntityManager.cs
+++ Game/EntityManager.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using GameJam14.Game.Entity;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -27,6 +30,7 @@ internal class EntityManager {
         foreach ( Entity.Entity entity in _entities) {
             entity.Update(gameTime);
         }
+        this.UpdateTargets();
     }
 
     public void Draw(SpriteBatch spriteBatch) {
@@ -35,7 +39,45 @@ internal class EntityManager {
         }
     }
 
+    public void UpdateTargets() {
+        foreach ( Enemy enemy in Enemies() ) {
+            if (enemy.IsTargetInRange()) {
+                continue;
+            }
+            enemy.CurrentTarget = null;
+            Debug.WriteLine("Lost target");
+            if ( enemy.Target.Type == Target.TargetType.Player ) {
+                float distance = Vector2.Distance(enemy.Position, Player().Position);
+                if ( enemy.Target.TargetRange >= distance ) {
+                    enemy.CurrentTarget = Player();
+                    Debug.WriteLine("Targeting player");
+                }
+            } else if (enemy.Target.Type == Target.TargetType.Light) {
+                float closestDistance = float.MaxValue;
+                foreach ( Light light in Lights() ) {
+                        float distance = Vector2.Distance(enemy.Position, light.Position);
+                        if (enemy.Target.TargetRange >= distance && distance < closestDistance) {
+                            closestDistance = distance;
+                            enemy.CurrentTarget = light;
+                        }
+                }
+            }
+        }
+    }
+
     public void Reset() {
         _entities.Clear();
+    }
+
+    public IEnumerable<Enemy> Enemies() {
+        return _entities.OfType<Enemy>();
+    }
+
+    public IEnumerable<Light> Lights() {
+        return _entities.OfType<Light>();
+    }
+
+    public Player Player() {
+        return (Player) _entities.Find(entity => entity is Player);
     }
 }
