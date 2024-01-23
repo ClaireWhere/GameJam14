041ccb771157d3426f5d490da2669fad00738625
ClaireWhere <clairewhere4@gmail.com>
2024-01-23 12:11:52 -0500

    feat: add collision effect

---
+++ Game/Entity/EntitySystem/CollisionEffect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam14.Game.Entity.EntitySystem;
+internal class CollisionEffect
+{
+    public enum EntityInteraction
+    {
+        EnemyPlayer,
+        PlayerEnemy,
+        ProjectileEnemy,
+        ProjectilePlayer,
+        ProjectileProjectile,
+        EnemyLight,
+        PlayerLight,
+        CloudPlayer,
+        CloudEnemy
+    }
+    public CollisionEffect(Action<Entity, Entity> effect)
+    {
+        this.Effect = effect;
+    }
+
+    public Action<Entity, Entity> Effect { get; protected set; }
+}
