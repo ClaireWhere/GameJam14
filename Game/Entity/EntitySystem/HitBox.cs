8df5ad528e25fae1a71edc9a5ec7a55c58ba6fe3
ClaireWhere <clairewhere4@gmail.com>
2024-02-03 01:18:09 -0500

    feat: create HitBox
    - Create new class `HitBox` to handle hitbox functionality for game entities
    - Include properties for Shape and Offset. Add two constructors - one with shape and offset, one with only shape, defaulting to zero offset.
    - Add `SetPosition(Vector2 position)` method to set the position of the `Shape`
    - Add method `CollidesWith(HitBox hitbox)` to check if the current `HitBox` collides with another `HitBox`
    - Override `ToString()` method
    - Add method `UpdateScale(float scale)` to update the scale of the `Shape` and the `Offset`

---
+++ Game/Entity/EntitySystem/HitBox.cs
@@ -0,0 +1,42 @@
+﻿// Ignore Spelling: hitbox
+
+using Microsoft.Xna.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameJam14.Game.Entity.EntitySystem;
+public class HitBox {
+    public Shape.Shape Shape { get; set; }
+    public Vector2 Offset { get; set; }
+
+    public HitBox(Shape.Shape shape, Vector2 offset) {
+        this.Shape = shape;
+        this.Offset = offset * shape.Scale;
+    }
+
+    public HitBox(Shape.Shape shape) {
+        this.Shape = shape;
+        this.Offset = Vector2.Zero;
+    }
+
+    public void SetPosition(Vector2 position) {
+        this.Shape.Position = position - this.Offset;
+    }
+
+    public bool CollidesWith(HitBox hitbox) {
+        return this.Shape.Intersects(hitbox.Shape);
+    }
+
+    public override string ToString() {
+        return "HitBox: " + this.Shape + " | Offset: " + this.Offset.ToString();
+    }
+
+    public void UpdateScale(float scale) {
+        this.Shape.Scale = scale;
+        this.Offset *= scale;
+    }
+}
